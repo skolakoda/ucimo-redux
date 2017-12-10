3f52bdabf851b1224c206b5c76ef09fdef38a341
Damjan Pavlica <mudroljub@gmail.com>
2017-12-10 03:16:39 +0100

    refaktor

---
+++ 25-todo-app-react/reduktori.js
@@ -0,0 +1,49 @@
+const {combineReducers} = Redux
+
+/* REDUKTORI */
+
+const addTodo = action => {
+  return {
+    id: action.id,
+    text: action.text,
+    completed: false
+  }
+}
+
+const toggleTodo = (todo, action) => {
+  if (todo.id !== action.id) return todo
+  return {
+    ...todo,
+    completed: !todo.completed
+  }
+}
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        addTodo(action)
+      ]
+    case 'TOGGLE_TODO':
+      return state.map(t => toggleTodo(t, action))
+    default:
+      return state
+  }
+}
+
+const visibilitiFilter = (state = 'SHOW_ALL', action) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter
+    default:
+      return state
+  }
+}
+
+/* KOMPOZITNI REDUKTOR */
+
+const todoApp = combineReducers({
+  todos,
+  visibilitiFilter
+})
